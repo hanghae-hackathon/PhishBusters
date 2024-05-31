bfa6479a13028a80e459b282bb97b77c3a2ecf8d
rladydqls99 <rladydqls99@naver.com>
2024-06-01 03:39:57 +0900

    icons

---
+++ client/src/components/icons.jsx
@@ -0,0 +1,26 @@
+import { IoCall } from 'react-icons/io5';
+import { AiOutlineAudioMuted } from 'react-icons/ai';
+import { IoIosKeypad } from 'react-icons/io';
+import { IoIosWifi } from 'react-icons/io';
+import { IoPersonAddSharp } from 'react-icons/io5';
+import { BiSolidVideoPlus } from 'react-icons/bi';
+import { AiOutlineSound } from 'react-icons/ai';
+import { TbMessageCircle2 } from 'react-icons/tb';
+import { MdOutlineVoicemail } from 'react-icons/md';
+
+export const Icons = () => {
+  return (
+    <>
+      <MdOutlineVoicemail />
+      <TbMessageCircle2 />
+      <IoIosWifi />
+      <IoCall />
+      <IoCall />
+      <AiOutlineAudioMuted />
+      <IoIosKeypad />
+      <IoPersonAddSharp />
+      <BiSolidVideoPlus />
+      <AiOutlineSound />
+    </>
+  );
+};
