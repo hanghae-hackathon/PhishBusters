52d36cd4f190ec6f6291bd9b58bb971ffb866948
rladydqls99 <rladydqls99@naver.com>
2024-06-01 06:31:28 +0900

    feat: 녹음 기능 구현

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import Audio from '../../components/Audio';
 
 const Home = () => {
-  return <HomeBox></HomeBox>;
+  const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(Date.now()), 1000);
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <HomeBox>
+      <Time>{new Date(time).toLocaleTimeString('it-IT')}</Time>
+      <Audio />
+    </HomeBox>
+  );
 };
 
 const HomeBox = styled.div`
   height: 100vh;
-  background-image: url(/src/assets/images/background/call.jpg);
+  background-image: url(/src/assets/images/background/home.jpg);
   background-size: cover;
   overflow: hidden;
 `;
 
+const Time = styled.h1`
+  color: white;
+  font-size: 3rem;
+  text-align: center;
+  margin-top: 20vh;
+  transform: translateY(-50%);
+`;
 export default Home;
