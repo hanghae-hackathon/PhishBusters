8fee889d8bfc90206e1ee1dbd76306581277a43b
ddevkim <ddevkim@gmail.com>
2024-06-01 03:43:16 +0900

    feat:call to stt

---
+++ server/functions/callSpeechToText.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const fs = require("fs");
+const speech = require("@google-cloud/speech");
+const client = new speech.SpeechClient({
+  projectId: 112146890057148407407,
+});
+
+async function transcribeAudio() {
+  const fileName = "../../ai/sample.wav";
+  const file = fs.readFileSync(fileName);
+  const audioBytes = file.toString("base64");
+
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: "LINEAR16",
+    sampleRateHertz: 16000,
+    languageCode: "en-US",
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+  const [response] = await client.recognize(request);
+  const transcription = response.results
+    .map((result) => result.alternatives[0].transcript)
+    .join("\n");
+  console.log(`Transcription: ${transcription}`);
+}
+
+transcribeAudio().catch(console.error);
